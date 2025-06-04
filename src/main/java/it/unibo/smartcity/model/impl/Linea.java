10d6a2354880af89dffba621f0574cd884d2832a
Nicolas Tazzieri <nicolas.tazzieri@studio.unibo.it>
2025-06-04 14:21:36 +0200

    feat: update Linea

--- src/main/java/it/unibo/smartcity/model/impl/Linea.java
+++ src/main/java/it/unibo/smartcity/model/impl/Linea.java
@@ -1,22 +1,25 @@
 package it.unibo.smartcity.model.impl;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 import java.util.Date;
+import java.util.Optional;
 
 public class Linea {
 
     private final String codiceLinea;
     private final int tempoPercorrenza;
-    private final Date inizioValidita;
-    private final Date fineValidita;
-    private final String attiva;
+    private final Optional<Date> inizioValidita;
+    private Optional<Date> fineValidita;
+    private boolean attiva;
     private final int codiceTipoMezzo;
 
-    public Linea(String codiceLinea, int tempoPercorrenza, Date inizioValidita, Date fineValidita, String attiva,
+    public Linea(String codiceLinea, int tempoPercorrenza, Date inizioValidita, Date fineValidita, boolean attiva,
             int codiceTipoMezzo) {
         this.codiceLinea = codiceLinea;
         this.tempoPercorrenza = tempoPercorrenza;
-        this.inizioValidita = inizioValidita;
-        this.fineValidita = fineValidita;
+        this.inizioValidita = Optional.of(inizioValidita);
+        this.fineValidita = Optional.of(fineValidita);
         this.attiva = attiva;
         this.codiceTipoMezzo = codiceTipoMezzo;
     }
@@ -29,15 +32,15 @@ public class Linea {
         return tempoPercorrenza;
     }
 
-    public Date getInizioValidita() {
+    public Optional<Date> getInizioValidita() {
         return inizioValidita;
     }
 
-    public Date getFineValidita() {
+    public Optional<Date> getFineValidita() {
         return fineValidita;
     }
 
-    public String getAttiva() {
+    public boolean getAttiva() {
         return attiva;
     }
 
@@ -45,4 +48,14 @@ public class Linea {
         return codiceTipoMezzo;
     }
 
+    public void setAttiva(boolean attiva) {
+        this.attiva = attiva;
+    }
+
+    public void setFineValidita(Date fineValidita) {
+        checkArgument(fineValidita != null, "La data di fine validità non può essere null");
+        this.fineValidita.ifPresent(date -> checkArgument(date.before(fineValidita),
+                "La data di fine validità deve essere successiva alla data attuale"));
+        this.fineValidita = Optional.of(fineValidita);
+    }
 }
