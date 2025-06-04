17de785fca4e1d03669596cbe23f53ac822dc603
Nicolas Tazzieri <nicolas.tazzieri@studio.unibo.it>
2025-06-04 14:11:33 +0200

    feat: add methods in ContenutoHub

--- src/main/java/it/unibo/smartcity/model/impl/ContenutoHub.java
+++ src/main/java/it/unibo/smartcity/model/impl/ContenutoHub.java
@@ -1,11 +1,12 @@
 package it.unibo.smartcity.model.impl;
+import static com.google.common.base.Preconditions.checkArgument;
 
 public class ContenutoHub {
     private final int codiceContenuto;
     private final int codiceHub;
     private final int postiMax;
-    private final int postiDisponibili;
     private final String descrizione;
+    private int postiDisponibili;
 
     public ContenutoHub(int codiceContenuto, int codiceHub, int postiMax, int postiDisponibili, String descrizione) {
         this.codiceContenuto = codiceContenuto;
@@ -33,6 +34,23 @@ public class ContenutoHub {
 
     public String getDescrizione() {
         return descrizione;
+    }
+
+    public void setPostiDisponibili(int postiDisponibili) {
+        checkArgument(postiDisponibili >= 0 && postiDisponibili <= postiMax, "I posti disponibili devono essere tra 0 e il massimo consentito.");
+        this.postiDisponibili = postiDisponibili;
+    }
+
+    public void addPosto() {
+        if (this.postiDisponibili < this.postiMax) {
+            this.postiDisponibili++;
+        }
+    }
+
+    public void removePosto() {
+        if (this.postiDisponibili > 0) {
+            this.postiDisponibili--;
+        }
     }
 
 }
