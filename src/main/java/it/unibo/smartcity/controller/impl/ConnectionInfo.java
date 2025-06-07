7fbdf6bac6656b97e7f470d27f7b92ca13a146e3
Enrico Bartocetti <enrico.bartocetti@studio.unibo.it>
2025-06-07 17:54:17 +0200

    Removed ConnectionInfo

--- src/main/java/it/unibo/smartcity/controller/impl/ConnectionInfo.java
+++
@@ -1,9 +0,0 @@
-package it.unibo.smartcity.controller.impl;
-
-class ConnectionInfo {
-    public static final String DB_NAME = "smart_city";
-    public static final String USER = "";
-    public static final String PASSWORD = "";
-
-    private ConnectionInfo() { }
-}
