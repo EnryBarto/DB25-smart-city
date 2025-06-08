a6ee79596280d80c4244ca19a6a13158971ac52e
Enrico Bartocetti <enrico.bartocetti@studio.unibo.it>
2025-06-08 15:25:35 +0200

    feat: Added message dialog to insert db credentials

--- src/main/java/it/unibo/smartcity/controller/impl/ControllerImpl.java
+++ src/main/java/it/unibo/smartcity/controller/impl/ControllerImpl.java
@@ -4,9 +4,13 @@ import java.sql.Connection;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import com.google.common.base.Preconditions;
 
 import it.unibo.smartcity.controller.api.Controller;
+import it.unibo.smartcity.data.DAOException;
 import it.unibo.smartcity.data.DAOUtils;
 import it.unibo.smartcity.data.InfoLinea;
 import it.unibo.smartcity.model.impl.LineaImpl;
@@ -17,7 +21,46 @@ import it.unibo.smartcity.view.api.View.SignupData;
 public class ControllerImpl implements Controller {
 
     private final Set<View> views = new HashSet<>();
-    private final Connection connection = DAOUtils.localMySQLConnection(ConnectionInfo.DB_NAME, ConnectionInfo.USER, ConnectionInfo.PASSWORD);
+    private Connection connection;
+
+    public ControllerImpl () {
+        var fake = new JFrame();
+        fake.setVisible(true);
+        // Crea i campi di input
+        javax.swing.JTextField dbField = new javax.swing.JTextField(20);
+        javax.swing.JTextField userField = new javax.swing.JTextField(20);
+        javax.swing.JPasswordField passField = new javax.swing.JPasswordField(20);
+
+        Object[] message = {
+            "Database:", dbField,
+            "Utente:", userField,
+            "Password:", passField
+        };
+
+        int option = javax.swing.JOptionPane.showConfirmDialog(
+            fake, message, "Parametri connessione DB", javax.swing.JOptionPane.OK_CANCEL_OPTION);
+
+        String dbName, user, pass;
+        if (option == javax.swing.JOptionPane.OK_OPTION) {
+            dbName = dbField.getText();
+            user = userField.getText();
+            pass = new String(passField.getPassword());
+        } else {
+            // Default o chiudi applicazione
+            dbName = "";
+            user = "";
+            pass = "";
+            System.exit(0);
+        }
+        try {
+            this.connection = DAOUtils.localMySQLConnection(dbName, user, pass);
+        } catch (DAOException e) {
+            JOptionPane.showMessageDialog(fake, "Connessione non riuscita!\n" + e.getMessage(), "Errore", JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        } finally {
+            fake.setVisible(false);
+        }
+    }
 
     @Override
     public void attachView(final View v) {
