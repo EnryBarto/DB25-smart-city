a1e567a76a247519b816da8ee01b2f925c996e98
Nicolas Tazzieri <nicolas.tazzieri@studio.unibo.it>
2025-07-02 09:02:24 +0200

    feat: added other data to sql

---
+++ src/main/sql/other_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO INSERT INTO FERMATE (nome, indirizzo_via, indirizzo_comune, indirizzo_civico, indirizzo_cap, longitudine, latitudine) VALUES
+('Pianventena (Centro)', 'Morciano', 'San Giovanni in Marignano', '2080', 47842, '43.9272198', '12.6867276'),
+('S. G. in Marignano 6 (C.Sportivo)', 'Pianventena', 'San Giovanni in Marignano', '6', 47842, '43.936031', '12.7020643'),
+('S. G. in Marignano 4 (Comune)', 'Roma', 'San Giovanni in Marignano', '4', 47842, '43.9390542', '12.7133298'),
+('Morciano (Centro Studi)', 'Largo Centro Studi', 'Morciano di Romagna', '6', 47833, '43.9161903', '12.6490851');
